6c98a8b2fa6cd595bed4829dad427fd202206c94
Kiuuuwu <frykowska.ka@gmail.com>
2024-03-13 17:40:01 +0100

    sraka

---
+++ wektor/cmake-build-debug/CMakeFiles/Wektory.dir/Ray.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/Wektory.dir/Ray.cpp.obj: \
+ E:\politechnika\magister\fotorealistyczna_grafika_komputerowa\awrfvSEDSVzd\Fotorealizm\wektor\Ray.cpp \
+ E:\politechnika\magister\fotorealistyczna_grafika_komputerowa\awrfvSEDSVzd\Fotorealizm\wektor\Ray.h
---
+++ wektor/cmake-build-debug/CMakeFiles/Wektory.dir/Sphere.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/Wektory.dir/Sphere.cpp.obj: \
+ E:\politechnika\magister\fotorealistyczna_grafika_komputerowa\awrfvSEDSVzd\Fotorealizm\wektor\Sphere.cpp \
+ E:\politechnika\magister\fotorealistyczna_grafika_komputerowa\awrfvSEDSVzd\Fotorealizm\wektor\Sphere.h
