f6bd48c8d7c628a9c6dec620ceeab627b01edd75
maciekr44 <maciekr@onet.eu>
2024-03-13 18:02:16 +0100

    Sfera

--- wektor/Sphere.cpp
+++ wektor/Sphere.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "Sphere.h"
+
+Sphere::Sphere() {
+}
+
+const Vector &Sphere::getCenter() const {
+    return Center_;
+}
+
+void Sphere::setCenter(const Vector &center) {
+    Center_ = center;
+}
+
+float Sphere::getRadius() const {
+    return Radius_;
+}
+
+void Sphere::setRadius(float radius) {
+    Radius_ = radius;
+}
--- wektor/Sphere.h
+++ wektor/Sphere.h
@@ -4,10 +4,36 @@
 
 #ifndef WEKTORY_SPHERE_H
 #define WEKTORY_SPHERE_H
-
+#include "Vector.h"
+#include "Ray.h"
 
 class Sphere {
+    public:
+        Sphere();
+        Sphere(float r);
+        Sphere(Vector v);
+        Sphere(Vector v, float r);
 
+        bool Hit(Ray ray, float t_min, float t_max) const;
+
+        Vector Center() const { return Center_; }
+        void Center(Vector o) { Center_ = o; }
+        float Radius() const { return Radius_;}
+        void Radius(float r) {Radius_ = r;}
+
+        friend std::ostream& operator <<(std::ostream& os, const Sphere& sph);
+
+    const Vector &getCenter() const;
+
+    void setCenter(const Vector &center);
+
+    float getRadius() const;
+
+    void setRadius(float radius);
+
+private:
+        Vector Center_;
+        float Radius_;
 };
 
 
