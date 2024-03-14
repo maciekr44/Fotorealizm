0aaa46215c494cace387ec58a129a21b50dd7194
Kiuuuwu <frykowska.ka@gmail.com>
2024-03-14 12:54:17 +0100

    siema

---
+++ wektor/cmake-build-debug/CMakeFiles/Wektory.dir/Plane.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/Wektory.dir/Plane.cpp.obj: \
+ E:\politechnika\magister\fotorealistyczna_grafika_komputerowa\fotorealistyczna\Fotorealizm\wektor\Plane.cpp \
+ E:\politechnika\magister\fotorealistyczna_grafika_komputerowa\fotorealistyczna\Fotorealizm\wektor\Plane.h
