6aa19a769073dfee743da01a0bde696046229822
maciekr44 <maciekr@onet.eu>
2024-03-14 13:52:49 +0100

    zmiany

--- wektor/cmake-build-debug/CMakeFiles/Wektory.dir/Plane.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Wektory.dir/Plane.cpp.obj: \
- E:\politechnika\magister\fotorealistyczna_grafika_komputerowa\3rsgtf\Fotorealizm\wektor\Plane.cpp \
- E:\politechnika\magister\fotorealistyczna_grafika_komputerowa\3rsgtf\Fotorealizm\wektor\Plane.h
