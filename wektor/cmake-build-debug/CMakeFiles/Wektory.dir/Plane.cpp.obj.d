0d8c6ac1cd925d6facc4875cb66cab2b9da2d6a1
Kiuuuwu <frykowska.ka@gmail.com>
2024-03-14 13:21:47 +0100

    GITIGNORRRR

--- wektor/cmake-build-debug/CMakeFiles/Wektory.dir/Plane.cpp.obj.d
+++ wektor/cmake-build-debug/CMakeFiles/Wektory.dir/Plane.cpp.obj.d
@@ -1,3 +1,3 @@
 CMakeFiles/Wektory.dir/Plane.cpp.obj: \
- C:\Users\Maciek\Desktop\Studia\Fotorealistyczna_Grafika_Komputerowa_\Fotorealizm\wektor\Plane.cpp \
- C:\Users\Maciek\Desktop\Studia\Fotorealistyczna_Grafika_Komputerowa_\Fotorealizm\wektor\Plane.h
+ E:\politechnika\magister\fotorealistyczna_grafika_komputerowa\3rsgtf\Fotorealizm\wektor\Plane.cpp \
+ E:\politechnika\magister\fotorealistyczna_grafika_komputerowa\3rsgtf\Fotorealizm\wektor\Plane.h
