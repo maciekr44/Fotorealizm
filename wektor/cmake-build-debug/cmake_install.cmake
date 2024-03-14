d5ef326d8a8077089c6d13d102c59e1c963ba5cc
maciekr44 <maciekr@onet.eu>
2024-03-14 12:57:37 +0100

    zmiany

--- wektor/cmake-build-debug/cmake_install.cmake
+++ wektor/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/politechnika/magister/fotorealistyczna_grafika_komputerowa/fotorealistyczna/Fotorealizm/wektor
+# Install script for directory: C:/Users/Maciek/Desktop/Studia/Fotorealistyczna_Grafika_Komputerowa_/Fotorealizm/wektor
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "E:/clion/CLion 2023.3.4/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.3.1/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/politechnika/magister/fotorealistyczna_grafika_komputerowa/fotorealistyczna/Fotorealizm/wektor/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Maciek/Desktop/Studia/Fotorealistyczna_Grafika_Komputerowa_/Fotorealizm/wektor/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
