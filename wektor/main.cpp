b1366209d8c68eeae780600064cef1aaa2721c9a
maciekr44 <maciekr@onet.eu>
2024-03-17 13:39:04 +0100

    zad 12

--- wektor/main.cpp
+++ wektor/main.cpp
@@ -35,10 +35,14 @@ int main() {
     Vector parallel = *new Vector(0, 20, -20);
     Ray r2 = *new Ray(ray_point, parallel);
 
-    cout << "\nZad1\n" <<endl;
+    Vector third_origin = *new Vector(8, -12, 0);
+    Vector third_destination = *new Vector(3, -2, 0);
+    Ray r3 = *new Ray(third_origin, third_destination);
+
+    cout << "\nZad 1\n" <<endl;
     cout << "Zaimplementowalismy klase wektor wraz ze wszystkimi dzialaniami." <<endl;
 
-    cout << "\nZad2\n" <<endl;
+    cout << "\nZad 2\n" <<endl;
     cout << "V1: " <<w1.showCoordinates() << endl;
     cout << "V2: " <<w2.showCoordinates() << endl;
     w1.add(w2);
@@ -51,33 +55,33 @@ int main() {
     cout <<"V2 + V1 = "<< w2.showCoordinates() << endl;
 
 
-    cout << "\nZad3\n" << "V1: " << w3.showCoordinates() << "\nV2: " << w4.showCoordinates() << endl;
+    cout << "\nZad 3\n" << "V1: " << w3.showCoordinates() << "\nV2: " << w4.showCoordinates() << endl;
     cout << "Kat wynosi: " << to_string(angle(w3, w4)) <<" stopni"<< endl;
 
-    cout << "\nZad4\n" << "V1: " << w5.showCoordinates() << "\nV2: " << w6.showCoordinates() << endl;
+    cout << "\nZad 4\n" << "V1: " << w5.showCoordinates() << "\nV2: " << w6.showCoordinates() << endl;
     Vector prostopadly = w5.cross(w6);
     cout << "Wektor prostopadly do obu poprzednich ma wspolrzedne: " << prostopadly.showCoordinates() << endl;
 
-    cout << "\nZad5\n";
+    cout << "\nZad 5\n";
     prostopadly.normalize();
     cout << "Znormalizowany poprzedni wektor prostopadly ma wspolrzedne: " << prostopadly.showCoordinates() << endl;
 
-    cout << "\nZad6\n";
+    cout << "\nZad 6\n";
     cout << "Zaimplementowalismy klasy promien, sfera i plaszczyzna." << endl;
 
-    cout << "\nZad7\n";
+    cout << "\nZad 7\n";
     cout << "S1: " <<s1.showCoordinates() << endl;
 
-    cout << "\nZad8\n";
+    cout << "\nZad 8\n";
     cout << "R1: " <<r1.showCoordinates() << endl;
 
-    cout << "\nZad9\n";
+    cout << "\nZad 9\n";
     cout << "R2: " <<r2.showCoordinates() << endl;
 
     cout << "\nZad3131\n" << endl;
     cout << "Kat wynosi: " << to_string(direction_angle(w90)) <<" stopni"<< endl;
 
-    cout << "\nZad10\n";
+    cout << "\nZad 10-11\n";
     std::pair<bool, std::pair<Vector, Vector>> result;
     result = s1.Hit(r1, 0, std::numeric_limits<float>::infinity());
     cout << "S z R1: ";
@@ -106,4 +110,18 @@ int main() {
         cout << "Brak kolizji." << endl;
     }
 
+    cout << "\nZad 12\n";
+    std::pair<bool, std::pair<Vector, Vector>> result3;
+    result3 = s1.Hit(r3, 0, std::numeric_limits<float>::infinity());
+    cout << "S z R3: ";
+    if (result3.first) {
+        cout << "Kolizja!" << endl;
+        Vector normal = result3.second.first; // Normalna w miejscu kolizji
+        Vector hit_point = result3.second.second; // Punkt kolizji
+        //cout << normal.showCoordinates() << endl;
+        cout << hit_point.showCoordinates() << endl;
+        // Możesz wykorzystać normalną i punkt kolizji do dalszych operacji, na przykład rysowania efektu kolizji
+    } else {
+        cout << "Brak kolizji." << endl;
+    }
 }
