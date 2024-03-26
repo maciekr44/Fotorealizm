73b4de5d732f794bcbcab5bfa19cddc2f7642995
Kiuuuwu <frykowska.ka@gmail.com>
2024-03-26 11:19:21 +0100

    antyaliasing jest teraz funkcja

--- wektor/OrtogonalCamera.h
+++ wektor/OrtogonalCamera.h
@@ -16,4 +16,77 @@ public:
     OrtogonalCamera(Vector _positionOrto, Vector _lookAtOrto, Vector _upOrto, float _leftOrto, float _rightOrto,
                     float _bottomOrto, float _topOrto, int _widthOrto, int _heightOrto);
 
+    static Intensity antyaliasingOrto(int sampling, float pointV, float pointU, float szerokosc, Ray raySampling, Sphere s1, Sphere s2, Ray rayOrthographic, Intensity Kolory[25], int iterator){
+        for(int t = 0; t<sampling; ++t){
+            // todo: po co to skoro nizej i tak nadpisuje??
+//                Vector samplingOrigin(0,pointV+(szerokosc*t),pointU);
+//                raySampling.setOrigin(samplingOrigin);
+//                Vector samplingDestination(100,pointV+(szerokosc*t),pointU);
+//                raySampling.setDestination(samplingDestination);
+            for(int p = 0; p<sampling; ++p){
+                Vector samplingOrigin(0,pointV+(szerokosc*t),pointU+(szerokosc*p));
+                raySampling.setOrigin(samplingOrigin);
+                Vector samplingDestination(100,pointV+(szerokosc*t),pointU+(szerokosc*p));
+                raySampling.setDestination(samplingDestination);
+                // Check for intersections with the sphere
+                IntersectionResult intersectionOrthographic = s1.Hit(rayOrthographic, 0, 900);  //tmax to ten nasz far plane
+                IntersectionResult intersectionOrthographicS2 = s2.Hit(rayOrthographic, 0,
+                                                                       900);  //tmax to ten nasz far plane
+
+
+                if (intersectionOrthographic.type == HIT && intersectionOrthographicS2.type != HIT) {
+                    Kolory[iterator] = intersectionOrthographic.color;
+                } else if (intersectionOrthographicS2.type == HIT && intersectionOrthographic.type != HIT) {
+                    Kolory[iterator] = intersectionOrthographicS2.color;
+                } else if (intersectionOrthographic.type == HIT && intersectionOrthographicS2.type == HIT) {
+                    // Choose the closest intersection
+                    if (intersectionOrthographic.distance < intersectionOrthographicS2.distance) {
+                        Kolory[iterator] = intersectionOrthographic.color;
+                    } else {
+                        Kolory[iterator] = intersectionOrthographicS2.color;
+                    }
+                } else {
+                    // Set background color
+                    Intensity bgColor(0, 1, 1);
+                    Kolory[iterator] = bgColor;
+                }
+//                if (Kolory[iterator].getRed() != 0 && Kolory[iterator].getGreen() != 1 && Kolory[iterator].getBlue() != 1)
+//                    std::cout << Kolory[iterator].getRed() << "; " << Kolory[iterator].getGreen() << "; "  << Kolory[iterator].getBlue() << std::endl;
+                iterator++; // wychodzi na to ze dotad kolory sa poprawnie
+
+
+            }
+        }
+        Intensity sredni;
+        Vector kolorki(0,0,0);
+        Vector suma(0,0,0);
+        for(int s = 0; s<sampling*sampling; ++s){
+            sredni = Kolory[s];
+            kolorki.setX(sredni.getRed());
+            kolorki.setY(sredni.getGreen());
+            kolorki.setZ(sredni.getBlue());
+
+            suma.add(kolorki);
+
+
+        }
+//        std::cout << suma.getX() << "; " << suma.getY() << "; "  << suma.getZ() << std::endl;
+
+        suma.div(25);
+
+
+//        std::cout << "po normlaizacjiajcsiq: " << suma.getX() << "; " << suma.getY() << "; "  << suma.getZ() << std::endl;
+
+
+        sredni.R(suma.getX());
+        sredni.G(suma.getY());
+        sredni.B(suma.getZ());
+
+//        sredni = sredni/25;
+//        std::cout << sredni.getRed() << "; " << sredni.getGreen() << "; "  << sredni.getBlue() << std::endl;
+
+        return (sredni);
+
+    }
+
 };
--- wektor/main.cpp
+++ wektor/main.cpp
@@ -24,174 +24,6 @@ float direction_angle(Vector v1) {
 }
 
 int main() {
-//    Vector w1 = *new Vector(1, 2, 3);
-//    Vector w2 = *new Vector(4, 5, 6);
-//    Vector w3 = *new Vector(0, 3, 0);
-//    Vector w4 = *new Vector(5, 5, 0);
-//    Vector w5 = *new Vector(4, 5, 1);
-//    Vector w6 = *new Vector(4, 1, 3);
-//
-//    Vector w90 = *new Vector(1, 1, 0);
-//
-//    Vector spher_point = *new Vector(0, 0, 0);
-//    Intensity color1 = *new Intensity(1,0,0);
-//    Intensity color2 = *new Intensity(0,1,0);
-//    Intensity color3 = *new Intensity(0,0,1);
-//    Sphere s1 = *new Sphere(spher_point, 10, color1);
-//
-//    Vector ray_point = *new Vector(0, 0, -20);
-//    Ray r1 = *new Ray(ray_point, spher_point);
-//
-//    Vector parallel = *new Vector(0, 40, -20);
-//    Ray r2 = *new Ray(ray_point, parallel);
-//
-//    Vector third_origin = *new Vector(8, -12, 0);
-//    Vector third_destination = *new Vector(3, -2, 0);
-//    Ray r3 = *new Ray(third_origin, third_destination);
-//
-//    Vector angle45 = *new Vector(0, 1, 1);
-//    Plane p1 = *new Plane(angle45, spher_point, color2);
-//
-//    cout << "\nZad 1\n" <<endl;
-//    cout << "Zaimplementowalismy klase wektor wraz ze wszystkimi dzialaniami." <<endl;
-//
-//    cout << "\nZad 2\n" <<endl;
-//    cout << "V1: " <<w1.showCoordinates() << endl;
-//    cout << "V2: " <<w2.showCoordinates() << endl;
-//    w1.add(w2);
-//
-//    cout <<"Suma wektorow:"<<endl;
-//    cout <<"V1 + V2 = "<< w1.showCoordinates() << endl;
-//
-//    w1.sub(w2);
-//    w2.add(w1);
-//    cout <<"V2 + V1 = "<< w2.showCoordinates() << endl;
-//
-//
-//    cout << "\nZad 3\n" << "V1: " << w3.showCoordinates() << "\nV2: " << w4.showCoordinates() << endl;
-//    cout << "Kat wynosi: " << to_string(angle(w3, w4)) <<" stopni"<< endl;
-//
-//    cout << "\nZad 4\n" << "V1: " << w5.showCoordinates() << "\nV2: " << w6.showCoordinates() << endl;
-//    Vector prostopadly = w5.cross(w6);
-//    cout << "Wektor prostopadly do obu poprzednich ma wspolrzedne: " << prostopadly.showCoordinates() << endl;
-//
-//    cout << "\nZad 5\n";
-//    prostopadly.normalize();
-//    cout << "Znormalizowany poprzedni wektor prostopadly ma wspolrzedne: " << prostopadly.showCoordinates() << endl;
-//
-//    cout << "\nZad 6\n";
-//    cout << "Zaimplementowalismy klasy promien, sfera i plaszczyzna." << endl;
-//
-//    cout << "\nZad 7\n";
-//    cout << "S1: " <<s1.showCoordinates() << endl;
-//
-//    cout << "\nZad 8\n";
-//    cout << "R1: " <<r1.showCoordinates() << endl;
-//
-//    cout << "\nZad 9\n";
-//    cout << "R2: " <<r2.showCoordinates() << endl;
-//
-//    cout << "\nZad 10-11\n";
-//    IntersectionResult result1;
-//    cout << "S z R1: " << endl;
-//    result1 = s1.Hit(r1, 0, std::numeric_limits<float>::infinity());
-//
-//    IntersectionResult result2;
-//    cout << "S z R2: " << endl;
-//    result2 = s1.Hit(r2, 0, std::numeric_limits<float>::infinity());
-//
-//    cout << "\nZad 12\n";
-//    IntersectionResult result3;
-//    cout << "S z R3: "  << endl;
-//    result3 = s1.Hit(r3, 0, std::numeric_limits<float>::infinity());
-//
-//
-//    cout << "\nZad 13-14";
-//    IntersectionResult int_res = p1.Intersects(r2, 200);
-//    cout << "P1: " <<p1.showCoordinates() << endl << "punkt przeciecia z r2:" << int_res.LPOINT.showCoordinates() << endl;
-//
-//    cout << "\nZad 15\n";
-//    Vector pktA = *new Vector(0, 0, 0);
-//    Vector pktB = *new Vector(1, 0, 0);
-//    Vector pktC = *new Vector(0, 1, 0);
-//    Triangle t1 = *new Triangle(pktA, pktB, pktC, color3);
-//
-//    cout << "Przypadek 1:\n";
-//    Vector pkt1 = *new Vector(-1, 0.5, 0);
-//    Vector pkt2 = *new Vector(1, 0.5, 0);
-//    Ray ray1 = *new Ray(pkt1, pkt2);
-//
-//    t1.IntersectTriangle(ray1, pktA, pktB, pktC);
-//
-//    cout << "Przypadek 2:\n";
-//    Vector pkt3 = *new Vector(2, -1, 0);
-//    Vector pkt4 = *new Vector(2, 2, 0);
-//    Ray ray2 = *new Ray(pkt3, pkt4);
-//
-//    t1.IntersectTriangle(ray2, pktA, pktB, pktC);
-//
-//    cout << "Przypadek 3:\n";
-//    Vector pkt5 = *new Vector(0, 0, -1);
-//    Vector pkt6 = *new Vector(0, 0, 1);
-//    Ray ray3 = *new Ray(pkt5, pkt6);
-//
-//    t1.IntersectTriangle(ray3, pktA, pktB, pktC);
-//
-//    cout << "\nZad 1\n";
-//    Intensity li1 = *new Intensity(0.2, 0.2, 0.2);
-//    Intensity li2 = *new Intensity(0.1, 0.1, 0.1);
-//    cout << li1 << ", "<<li2<< endl;
-//    li1 += li2;
-//    cout << li1 << ", " << li2 << endl;
-//    li2 = li1/4;
-//    cout << li1 << ", " << li2 << endl;
-//    li1 *= 2;
-//    cout << li1 << ", " << li2 << endl;
-//    li1 /=  3;
-//    cout << li1 << ", " << li2 << endl;
-//
-//    Vector Up = *new Vector(0, 0, 1);
-//    PerspectiveCamera pc1 = *new PerspectiveCamera(ray_point, spher_point, spher_point, parallel, Up, 2 );
-//    cout << "PC1: " <<pc1.showCoordinates() << endl;
-
-    // rysowanie kolka sfml
-//    sf::RenderWindow window(sf::VideoMode(200,200),"siema");
-//    sf::CircleShape shape(100.f);
-//    shape.setFillColor(sf::Color::Blue);
-//    while(window.isOpen()){
-//        sf::Event event;
-//        while(window.pollEvent(event)){
-//            if (event.type == sf::Event::Closed)
-//                window.close();
-//
-//        }
-//        window.clear();
-//        window.draw(shape);
-//        window.display();
-//    }
-
-//    sf::Drawable img = *new Image(200, 200);
-//    Intensity color = *new Intensity(1,0,0);
-//
-//    for (int i = 0; i++; img.height){
-//        for (int j = 0; j++; img.width){
-//            img.setPixel(i, j, color);
-//        }
-//    }
-//
-//// SFML
-//    sf::RenderWindow window(sf::VideoMode(img.width,img.height),"siema");
-//    while(window.isOpen()){
-//        sf::Event evnt;
-//        while(window.pollEvent(evnt)){
-//            if (evnt.type == sf::Event::Closed)
-//                window.close();
-//
-//        }
-//        window.clear();
-//        window.draw(img);
-//        window.display();
-//    }
 
     Vector spher_point = *new Vector(0, 0, 0);
     Vector spher_point2 = *new Vector(28, 10, 0);
@@ -208,7 +40,7 @@ int main() {
     cout<<s1.getColor().getBlue()<<endl;
 
     // Create an image with given width and height
-    Image image(200, 200);
+    Image image(500, 500);
 
     Vector cameraPositionOrto(-40, 0, 0);  // Set the camera position
     Vector lookAtOrto(-30, 0, 0);  // Set the point to look at
@@ -241,8 +73,6 @@ int main() {
     for (int y = 0; y < image.height; ++y) {
         for (int x = 0; x < image.width; ++x) {
 
-            cout << rayOrthographic.showCoordinates() << endl;
-
             kierunek.setOrigin(cameraPositionOrto);
             kierunek.setDestination(lookAtOrto);
 //            kierunek = Ray(cameraPositionOrto, lookAtOrto);
@@ -251,7 +81,7 @@ int main() {
             float v = (-1.0f + 2.0f * y / (image.height - 1.0f)) * kierunek.getDistance();
             int sampling = 5;
             float szerokosc = ((kierunek.getDistance()*2)/image.width)/sampling;
-            cout<<"Szerokosc"<<szerokosc<<endl;
+//            cout<<"Szerokosc"<<szerokosc<<endl;
             float pointU = u - (2 * szerokosc);
             float pointV = v - (2 * szerokosc);
 //            cout<<"U "<<pointU<<endl;
@@ -281,6 +111,7 @@ int main() {
             rayOrthographic.setDestination(finish);
 
 
+            // ANTYALIASING
             int iterator = 0;
             Intensity Kolory[25];
 //            Ray raySampling = *new Ray(*new Vector(0, pointV, pointU), *new Vector(100, pointV, pointU));
@@ -289,80 +120,14 @@ int main() {
             Vector raySamplingDestination(100, pointV, pointU);
             raySampling.setOrigin(raySamplingDestination);
 
-            for(int t = 0; t<sampling; ++t){
-                raySampling.setOrigin(*new Vector(0,pointV+(szerokosc*t),pointU));
-                raySampling.setDestination(*new Vector(100,pointV+(szerokosc*t),pointU));
-                for(int p = 0; p<sampling; ++p){
-                    raySampling.setOrigin(*new Vector(0,pointV+(szerokosc*t),pointU+(szerokosc*p)));
-                    raySampling.setDestination(*new Vector(100,pointV+(szerokosc*t),pointU+(szerokosc*p)));
-                    // Check for intersections with the sphere
-                    IntersectionResult intersectionOrthographic = s1.Hit(rayOrthographic, 0, 900);  //tmax to ten nasz far plane
-                    IntersectionResult intersectionOrthographicS2 = s2.Hit(rayOrthographic, 0,
-                                                                           900);  //tmax to ten nasz far plane
 
-
-                    if (intersectionOrthographic.type == HIT && intersectionOrthographicS2.type != HIT) {
-                        Kolory[iterator] = intersectionOrthographic.color;
-                    } else if (intersectionOrthographicS2.type == HIT && intersectionOrthographic.type != HIT) {
-                        Kolory[iterator] = intersectionOrthographicS2.color;
-                    } else if (intersectionOrthographic.type == HIT && intersectionOrthographicS2.type == HIT) {
-                        // Choose the closest intersection
-                        if (intersectionOrthographic.distance < intersectionOrthographicS2.distance) {
-                            Kolory[iterator] = intersectionOrthographic.color;
-                        } else {
-                            Kolory[iterator] = intersectionOrthographicS2.color;
-                        }
-                    } else {
-                        // Set background color
-                        Intensity bgColor(0, 1, 1);
-                        Kolory[iterator] = bgColor;
-                        cout<<"TLO"<<Kolory[iterator]<<endl;
-                    }
-                    iterator++;
-
-
-                }
-            }
-            Intensity sredni;
-            for(int s = 0; s<sampling*sampling; ++s){
-                sredni += Kolory[s];
-            }sredni/25;
+            Intensity sredni = OrtogonalCamera::antyaliasingOrto(sampling, pointV, pointU, szerokosc, raySampling, s1, s2, rayOrthographic, Kolory, iterator);
+//            cout << sredni.getRed() << "; " << sredni.getGreen() << "; "  << sredni.getBlue() << endl;
             image.setPixel(x, y, sredni);
 
 
+            // ----------------------------------
 
-//
-//            Vector zwrot = kierunek.getDirection();
-//            float cameraEnd = zwrot.getX();
-//
-//            Vector finish = *new Vector(cameraEnd, v, u);
-//            float cameraPoint = cameraPositionOrto.getX();
-//            Vector start = *new Vector(cameraPoint, v, u);
-//
-//
-//            Ray rayOrthographic = *new Ray(start, finish);
-//            // Check for intersections with the sphere
-//           IntersectionResult intersectionOrthographic = s1.Hit(rayOrthographic, 0, 900);  //tmax to ten nasz far plane
-//            IntersectionResult intersectionOrthographicS2 = s2.Hit(rayOrthographic, 0,
-//                                                                   900);  //tmax to ten nasz far plane
-//
-//
-//            if (intersectionOrthographic.type == HIT && intersectionOrthographicS2.type != HIT) {
-//                image.setPixel(x, y, intersectionOrthographic.color);
-//            } else if (intersectionOrthographicS2.type == HIT && intersectionOrthographic.type != HIT) {
-//                image.setPixel(x, y, intersectionOrthographicS2.color);
-//            } else if (intersectionOrthographic.type == HIT && intersectionOrthographicS2.type == HIT) {
-//                // Choose the closest intersection
-//                if (intersectionOrthographic.distance < intersectionOrthographicS2.distance) {
-//                    image.setPixel(x, y, intersectionOrthographic.color);
-//                } else {
-//                    image.setPixel(x, y, intersectionOrthographicS2.color);
-//                }
-//            } else {
-//                // Set background color
-//                Intensity bgColor(0, 1, 1);
-//                image.setPixel(x, y, bgColor);
-//            }
         }
     }
 
@@ -470,4 +235,6 @@ int main() {
         window1.display();
     }
 }
+
+
 
